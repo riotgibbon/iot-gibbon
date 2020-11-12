f28ecd890b2ac570e89e6ce091acbcc844b14423
Toby Evans <toby.evans@arup.com>
2020-11-12 15:11:54 +0000

    switched loop

--- python/pi_home.py
+++ python/pi_home.py
@@ -181,12 +181,52 @@ device = get_device()
 # bme680Read = False
 # dht22Read = True
 
+
+today_last_time = "Unknown"
 while True:
-    temperature=""
-    pressure=""
-    humidity=""
-    clockReadings(temperature, humidity, pressure)
-    time.sleep(1)
+    now = datetime.datetime.now()
+    today_date = now.strftime("%d %b %y")
+    today_time = now.strftime("%H:%M:%S")
+    if today_time != today_last_time:
+        today_last_time = today_time
+        with canvas(device) as draw:
+            now = datetime.datetime.now()
+            today_date = now.strftime("%d %b %y")
+
+            margin = 4
+
+            cx = 30
+            cy = min(device.height, 64) / 2
+
+            left = cx - cy
+            right = cx + cy
+
+            hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
+            hrs = posn(hrs_angle, cy - margin - 7)
+
+            min_angle = 270 + (6 * now.minute)
+            mins = posn(min_angle, cy - margin - 2)
+
+            sec_angle = 270 + (6 * now.second)
+            secs = posn(sec_angle, cy - margin - 2)
+
+            draw.ellipse((left + margin, margin, right - margin, min(device.height, 64) - margin), outline="white")
+            draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
+            draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
+            draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
+            draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill="white", outline="white")
+            draw.text((2 * (cx + margin), cy - 8), today_date, fill="yellow")
+            draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
+
+    time.sleep(0.1)
+
+
+
+    # temperature=""
+    # pressure=""
+    # humidity=""
+    # clockReadings(temperature, humidity, pressure)
+    # time.sleep(0.1)
 
     # try:
     #     logger.info("starting bme680 read")

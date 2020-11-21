7527128f43d1d668b6c24622f4729ea7fe83ddbb
Toby Evans <toby.evans@arup.com>
2020-11-21 13:51:33 +0000

    nextPlantAt

--- python/microservices/hue/hue.py
+++ python/microservices/hue/hue.py
@@ -97,7 +97,8 @@ def postToLights(plantName, reading):
         lightInfo= str(b.get_light(lightId))
         body={}
         body['hue']=lightInfo
-        plantInfo ={'name':plantName, 'reading': reading, 'nextPlantAt': plantChangeTime}
+        nextPlantTime=plantChangeTime - datetime.now()
+        plantInfo ={'name':plantName, 'reading': reading, 'nextPlantAt': nextPlantTime}
         body['plant']=plantInfo
         client.publish('home/cmd/hue/tv/', str(body))
     except Exception:

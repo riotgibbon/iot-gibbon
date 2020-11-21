1d2620a9ad4f966d88931d4e70a151d0a4a7ce82
Toby Evans <toby.evans@arup.com>
2020-11-21 19:09:52 +0000

    fixed topic, parsed json

--- python/py_home_colour.py
+++ python/py_home_colour.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-
+import json
 
 
 
@@ -20,11 +20,13 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(f"home/cmd/hue/tv/")
+    client.subscribe(f"home/cmd/hue/tv")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(f"new message {msg.topic}: {str(msg.payload)}")
+    body = json.loads(str(msg.payload))
+    print(body)
 
         
 

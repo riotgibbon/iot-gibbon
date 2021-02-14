0619725491bae6c62de0b45b40b6d8f81c7e8c16
Toby Evans <toby.evans@arup.com>
2021-02-14 15:09:26 +0000

    added air quality to bme60

--- python/sensors/bme680/bme680_mqtt.py
+++ python/sensors/bme680/bme680_mqtt.py
@@ -3,7 +3,7 @@
 import time
 import board
 import requests
-import logging
+import loggingxx
 import os
 from datetime import datetime,timedelta
 import bme680
@@ -73,6 +73,39 @@ sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(150)
 sensor.select_gas_heater_profile(0)
 
+start_time = time.time()
+curr_time = time.time()
+burn_in_time = 300
+
+burn_in_data = []
+
+try:
+    # Collect gas resistance burn-in values, then use the average
+    # of the last 50 values to set the upper limit for calculating
+    # gas_baseline.
+    print('Collecting gas resistance burn-in data for 5 mins\n')
+    while curr_time - start_time < burn_in_time:
+        curr_time = time.time()
+        if sensor.get_sensor_data() and sensor.data.heat_stable:
+            gas = sensor.data.gas_resistance
+            burn_in_data.append(gas)
+            print('Gas: {0} Ohms'.format(gas))
+            time.sleep(1)
+
+    gas_baseline = sum(burn_in_data[-50:]) / 50.0
+
+    # Set the humidity baseline to 40%, an optimal indoor humidity.
+    hum_baseline = 40.0
+
+    # This sets the balance between humidity and gas reading in the
+    # calculation of air_quality_score (25:75, humidity:gas)
+    hum_weighting = 0.25
+
+    print('Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n'.format(
+        gas_baseline,
+        hum_baseline))
+
+
 
 broker="192.168.0.46"
 port=1883
@@ -106,7 +139,44 @@ while True:
             # 
             publish(mqttClient,"temperature",temperature)   
             publish(mqttClient,"humidity",humidity)  
-            publish(mqttClient,"pressure",pressure)       
+            publish(mqttClient,"pressure",pressure)  
+
+            if sensor.get_sensor_data() and sensor.data.heat_stable:
+                gas = sensor.data.gas_resistance
+                gas_offset = gas_baseline - gas
+
+                hum = sensor.data.humidity
+                hum_offset = hum - hum_baseline
+
+                # Calculate hum_score as the distance from the hum_baseline.
+                if hum_offset > 0:
+                    hum_score = (100 - hum_baseline - hum_offset)
+                    hum_score /= (100 - hum_baseline)
+                    hum_score *= (hum_weighting * 100)
+
+                else:
+                    hum_score = (hum_baseline + hum_offset)
+                    hum_score /= hum_baseline
+                    hum_score *= (hum_weighting * 100)
+
+                # Calculate gas_score as the distance from the gas_baseline.
+                if gas_offset > 0:
+                    gas_score = (gas / gas_baseline)
+                    gas_score *= (100 - (hum_weighting * 100))
+
+                else:
+                    gas_score = 100 - (hum_weighting * 100)
+
+                # Calculate air_quality_score.
+                air_quality_score = hum_score + gas_score
+
+                print('Gas: {0:.2f} Ohms,humidity: {1:.2f} %RH,air quality: {2:.2f}'.format(
+                    gas,
+                    hum,
+                    air_quality_score))  
+                
+                publish(mqttClient,"gas",gas)   
+                publish(mqttClient,"air_quality_score",air_quality_score)    
 
             if datetime.now() > ifftWriteTime:
                 try:

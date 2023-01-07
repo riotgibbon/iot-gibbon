3298764f1a89df7a6748b14673817eb5ff7a145d
riotgibbon <tobysevans@gmail.com>
2023-01-07 07:56:56 +0000

    optimised query, simplified message

--- aviation/planeLights.py
+++ aviation/planeLights.py
@@ -9,7 +9,7 @@ from rgbxy import Converter
 
 converter = Converter()
 
-sleepSeconds=20
+sleepSeconds=15
 
 CONNECTION = "postgres://postgres:password@192.168.0.46:5432/postgres"
 
@@ -54,30 +54,30 @@ while True:
         # print("getting data")
         query="""
     select * from (
-                    select icao,
-                            altitude,
-                            speed,
-                            distance,
-                            ts,
-                            updated,
-                            ROW_NUMBER() OVER (
-                                PARTITION BY icao
-                                ORDER BY ts desc) reading
+                      select icao,
+                             altitude,
+                             speed,
+                             distance,
+--                              ts,
+                             updated,
+                             ROW_NUMBER() OVER (
+                                 PARTITION BY icao
+                                 ORDER BY updated desc) reading
 
-                    from (
-                            select st_distance(ST_MakePoint(lon, lat) ::geography,
-                                                ST_MakePoint(-0.597207, 51.503572) ::geography) distance,
-                                    TO_TIMESTAMP(updated, 'YYYY-MM-DD HH24:MI:SS.MS')           ts,
-                                    *
-                            from flights
-                            where altitude>0 and lat>0
-                            and flightdate = CURRENT_DATE::TEXT
-                        ) f
-            where ts>CURRENT_TIMESTAMP - interval '2 minute'
-                )r
-where reading=1
-order by distance
-limit 1
+
+                      from (
+                               select st_distance(ST_MakePoint(lon, lat) ::geography,
+                                                  ST_MakePoint(-0.597207, 51.503572) ::geography) distance,
+                                      *
+                               from flights
+                               where altitude>0 and lat>0
+                                and flightdate = CURRENT_DATE::TEXT
+                                and updated > (CURRENT_TIMESTAMP - interval '2 minute')::TEXT
+                           ) f
+                  )r
+    where reading=1
+    order by distance
+    limit 1
         """
         cursor.execute(query)
         row = cursor.fetchone()
@@ -86,7 +86,7 @@ limit 1
         altitude=row[1]
         speed=row[2]
         distance=row[3]
-        updated=row[5]
+        updated=row[4]
         planeData={'icao': icao , 'altitude' : altitude, 'speed': speed, 'distance' : distance, 'updated': updated}
         print(planeData)
         maxDistance = 75000
@@ -95,25 +95,12 @@ limit 1
         hueMappedValue =int(mapRange (altitude, 500, 40000, hueLow, hueHigh))
         sat =int(mapRange (speed, 0, 600, 50,254))
 
-        bri =int(mapRange (maxDistance - distance, 0, maxDistance, 25,254))
+        bri =int(mapRange (maxDistance - distance, 0, maxDistance, 25,200))
         command =  {'transitiontime' : transitionTime,  'hue':  hueMappedValue, 'sat':sat, 'bri': bri}
         print(command)
         b.set_light(lightId,command)
-        # jsonMsg = json.dumps(planeData)
-        # client.publish('home/cmd/hue/plane', jsonMsg)
-
-        lightInfo= b.get_light(lightId)
-
-        xy = lightInfo['state']['xy']
-        x= xy[0]
-        y=xy[1]
-
-        hex = f"#{converter.xy_to_hex(x,y,bri)}"
-
-        hue= {'xy': xy, 'hex': hex, 'rgb':converter.xy_to_rgb(x,y,bri), 'bri': bri, 'hue': hueMappedValue }
-        # print(hue)
         body={}
-        body['hue']=hue #json.dumps(lightInfo)
+        body['hue']=command #json.dumps(lightInfo)
         
         body['plane']=  planeData
 

05d9353ba42769e6fe243d6ec502785095ba283b
riotgibbon <tobysevans@gmail.com>
2023-02-23 18:58:49 +0000

    thermal

--- python/microservices/thermal/readThermal.py
+++ python/microservices/thermal/readThermal.py
@@ -2,6 +2,19 @@ import paho.mqtt.client as mqtt
 import json
 import ST7789
 
+
+disp = ST7789.ST7789(
+    height=135 if display_type == "rect" else 240,
+    rotation=0 if display_type == "rect" else 90,
+    port=0,
+    cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
+    dc=9,
+    backlight=19,               # 18 for back BG slot, 19 for front BG slot.
+    spi_speed_hz=80 * 1000 * 1000,
+    offset_left=0 if display_type == "square" else 40,
+    offset_top=53 if display_type == "rect" else 0
+)
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
